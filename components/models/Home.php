cce5250b92f688cc3009301205e00fc12b51736c
Arthur MATHIS <arthur.mathis@fondation.local>
2024-05-31 15:09:02 +0200

    Mise en place de la recherche SQL des candidaturs -> pas Home

--- components/models/Home.php
+++ components/models/Home.php
@@ -5,20 +5,22 @@ require_once(MODELS.DS.'Model.php');
 class Home extends Model {
     public function getNontraiteeCandidature() {
         // On initialise la requête
-        $request = "";
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom, 
+        email_candidats AS Email, 
+        telephone_candidats AS Téléphone, 
+        intitule_sources AS Source
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources
+        WHERE c.Status_Candidatures = 'non-traitee'";
 
         // On lance la requête
-        $result = $this->get_request($request, $params=[], true, true);
+        $result = $this->get_request($request, $params=[]);
 
         // On retourne le rôle
         return $result;
     }
 }
-
-/*
-SELECT nom, prenom, email, telephone
-FROM `candidatures` as c
-INNER JOIN candidats as i on c.Id_Candidats = i.Id
-INNER JOin postes as p on c.Id_Postes = p.Id
-INNER JOIN sources as s on c.Id_Sources = s.Id
-*/
