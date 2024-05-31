674bcab7cca80a92d9ec35473686971631c80e31
Arthur MATHIS <arthur.mathis@fondation.local>
2024-05-31 15:25:59 +0200

    Ajout de méthode de requête à la base de données (Home.php) permettant de récupérer les candidatures (format complet ou réduit) selon leur status

--- components/controllers/HomeController.php
+++ components/controllers/HomeController.php
@@ -10,101 +10,31 @@ class HomeController extends Controller {
     }
 
     function displayHome() {
+        $items = $this->Model->getNontraiteesCandidatures();
         $dashboard = [
             [
-                'titre' => 'Candidatures en attente',
-                'content' => [
-                    [
-                        'Nom' => 'Rousseau',
-                        'Prenom' => 'Chloe',
-                        'Poste' => 'Radiologue'
-                    ],
-                    [
-                        'Nom' => 'Petit',
-                        'Prenom' => 'Paul',
-                        'Poste' => 'Pédiatre'
-                    ],
-                    [
-                        'Nom' => 'Moreau',
-                        'Prenom' => 'Sophie',
-                        'Poste' => 'Pharmacien'
-                    ],
-                    [
-                        'Nom' => 'David',
-                        'Prenom' => 'Thomas',
-                        'Poste' => 'Oncologue'
-                    ]
-                ],
+                'titre' => 'Candidatures en Attente', 
+                'content' => $this->Model->getReductEnattenteCandidatures(), 
                 'nb_item_max' => 4,
                 'link_add' => null,
                 'link_consult' => null
-            ], 
+            ],
             [
-                'titre' => 'Candidatures en attente',
-                'content' => [
-                    [
-                        'Nom' => 'Rousseau',
-                        'Prenom' => 'Chloe',
-                        'Poste' => 'Radiologue'
-                    ],
-                    [
-                        'Nom' => 'Petit',
-                        'Prenom' => 'Paul',
-                        'Poste' => 'Pédiatre'
-                    ],
-                    [
-                        'Nom' => 'Moreau',
-                        'Prenom' => 'Sophie',
-                        'Poste' => 'Pharmacien'
-                    ],
-                    [
-                        'Nom' => 'David',
-                        'Prenom' => 'Thomas',
-                        'Poste' => 'Oncologue'
-                    ]
-                ],
+                'titre' => 'Offres en cours', 
+                'content' => [], 
                 'nb_item_max' => 2,
                 'link_add' => null,
                 'link_consult' => null
             ],
             [
-                'titre' => 'Candidatures en attente',
-                'content' => [
-                    [
-                        'Nom' => 'Rousseau',
-                        'Prenom' => 'Chloe',
-                        'Poste' => 'Radiologue'
-                    ],
-                    [
-                        'Nom' => 'Petit',
-                        'Prenom' => 'Paul',
-                        'Poste' => 'Pédiatre'
-                    ],
-                    [
-                        'Nom' => 'Moreau',
-                        'Prenom' => 'Sophie',
-                        'Poste' => 'Pharmacien'
-                    ],
-                    [
-                        'Nom' => 'David',
-                        'Prenom' => 'Thomas',
-                        'Poste' => 'Oncologue'
-                    ]
-                ],
+                'titre' => 'Rendez-vous programmés', 
+                'content' => [], 
                 'nb_item_max' => 2,
                 'link_add' => null,
                 'link_consult' => null
-            ]
+            ],
         ];
-
-        $items = $this->Model->getNontraiteeCandidature();
-        $jsonItems = json_encode($items);
-        echo "<script>
-            const items = $jsonItems;
-            console.table(items);
-        </script>";
-
-        
+    
         return $this->View->getContent("Candidatures non-traitée", $items, $dashboard);
     }
 }
--- components/models/Home.php
+++ components/models/Home.php
@@ -3,7 +3,43 @@
 require_once(MODELS.DS.'Model.php');
 
 class Home extends Model {
-    public function getNontraiteeCandidature() {
+    public function getCandidatures(){
+        // On initialise la requête
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom, 
+        email_candidats AS Email, 
+        telephone_candidats AS Téléphone, 
+        intitule_sources AS Source
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources";
+    
+        // On lance la requête
+        $result = $this->get_request($request);
+    
+        // On retourne le rôle
+        return $result;
+    }
+    public function getReductCandidatures(){
+        // On initialise la requête
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources";
+    
+        // On lance la requête
+        $result = $this->get_request($request);
+    
+        // On retourne le rôle
+        return $result;
+    }
+
+    public function getNontraiteesCandidatures() {
         // On initialise la requête
         $request = "SELECT intitule_postes AS Poste, 
         nom_candidats AS Nom, 
@@ -16,10 +52,101 @@ class Home extends Model {
         INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
         INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources
         WHERE c.Status_Candidatures = 'non-traitee'";
-
+    
         // On lance la requête
-        $result = $this->get_request($request, $params=[]);
-
+        $result = $this->get_request($request);
+    
+        // On retourne le rôle
+        return $result;
+    }
+    public function getReductNontraiteesCandidatures() {
+        // On initialise la requête
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources
+        WHERE c.Status_Candidatures = 'non-traitee'";
+    
+        // On lance la requête
+        $result = $this->get_request($request);
+    
+        // On retourne le rôle
+        return $result;
+    }
+    public function getEnattenteCandidatures() {
+        // On initialise la requête
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom, 
+        email_candidats AS Email, 
+        telephone_candidats AS Téléphone, 
+        intitule_sources AS Source
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources
+        WHERE c.Status_Candidatures = 'en attente'";
+    
+        // On lance la requête
+        $result = $this->get_request($request);
+    
+        // On retourne le rôle
+        return $result;
+    }
+    public function getReductEnattenteCandidatures() {
+        // On initialise la requête
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources
+        WHERE c.Status_Candidatures = 'en attente'";
+    
+        // On lance la requête
+        $result = $this->get_request($request);
+    
+        // On retourne le rôle
+        return $result;
+    }
+    public function getTraiteeCandidatures() {
+        // On initialise la requête
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom, 
+        email_candidats AS Email, 
+        telephone_candidats AS Téléphone, 
+        intitule_sources AS Source
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources
+        WHERE c.Status_Candidatures = 'traitee'";
+    
+        // On lance la requête
+        $result = $this->get_request($request);
+    
+        // On retourne le rôle
+        return $result;
+    }
+    public function getReductTraiteeCandidatures() {
+        // On initialise la requête
+        $request = "SELECT intitule_postes AS Poste, 
+        nom_candidats AS Nom, 
+        prenom_candidats AS Prénom
+        FROM `candidatures` as c
+        INNER JOIN candidats as i on c.Cle_Candidats = i.Id_Candidats
+        INNER JOin postes as p on c.Cle_Postes = p.Id_Postes
+        INNER JOIN sources as s on c.Cle_Sources = s.Id_Sources
+        WHERE c.Status_Candidatures = 'traitee'";
+    
+        // On lance la requête
+        $result = $this->get_request($request);
+    
         // On retourne le rôle
         return $result;
     }
