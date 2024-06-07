05872b682c27cfe1623ec87d277138b04c755eec
Arthur Mathis <arthurmathis@MacBook-Pro-de-Arthur.local>
2024-06-07 23:05:32 +0200

    Refonte de la page de connexion avec animation du coeur ; ajout de la Roboto

--- layouts/formulaire_connexion.php
+++ layouts/formulaire_connexion.php
@@ -1,4 +1,7 @@
-<img src="layouts/assets\img\photo_log-in.jpg" alt="Illustration de l'hopithal">
+<div class="connexion-illustration">
+    <img src="layouts/assets/img/coeur.png" alt="Illustration de coeur">
+    <img src="layouts/assets/img/main.png" alt="Illustration de main">
+</div>
 <form method="post" action="index.php?login=connexion">
     <img src="layouts/assets/img/ico_diaconat_mulhouse.webp">
     <h2>Entrez votre identifiant et mot de passe pour accéder au site web</h2>
