6fa4134f1b3090f32d6da8f8f41de08c86748b7a
Arthur MATHIS <arthur.mathis@fondation.local>
2024-08-06 11:46:22 +0200

    Patch correctif du script d'autocomplétion

--- layouts/assets/scripts/models/objects/AutoComplet.js
+++ layouts/assets/scripts/models/objects/AutoComplet.js
@@ -2,7 +2,7 @@
  * @class AutoComplete
  * @classdesc Classe permettant de créer un champ d'auto-complétion.
  */
- class AutoComplete {
+class AutoComplete {
     /**
      * @constructor
      * @param {HTMLElement} inputElement - L'élément input sur lequel appliquer l'auto-complétion.
@@ -20,39 +20,13 @@
      * @description Initialise l'auto-complétion en ajoutant les écouteurs d'événements nécessaires.
      */
     createAutoComplete() {
+        // On construit les nouveaux éléments
         const parentDiv = this.inputElement.parentNode;
         const suggestionBox = parentDiv.querySelector('article');
 
-        this.inputElement.addEventListener('input', () => {
-            // On efface l'ancienne liste
-            this.closeAllLists();
-
-            // Si le champs est vide on s'arrête
-            if (!this.inputElement.value) return;
-
-            // On génère le tableau de suggestions selon la saisie de l'utilisateur
-            const filteredSuggestions = this.suggestions.filter(suggestion =>
-                suggestion.toLowerCase().trim().startsWith(this.inputElement.value.toLowerCase().trim())
-            );
-
-            // On test la présence de résultat
-            if (filteredSuggestions.length > 0) {
-                suggestionBox.classList.add('autocomplete-items');
-
-                // On génère les items de suggestion
-                filteredSuggestions.forEach(suggestion => {
-                    const item = document.createElement('div');
-                    item.innerHTML = suggestion;
-                    item.addEventListener('click', () => {
-                        this.inputElement.value = suggestion;
-                        this.inputElement.dispatchEvent(new Event('AutoCompleteSelect'));
-                        this.closeAllLists();
-                    });
-                    suggestionBox.appendChild(item);
-                });
-            }
-                
-        });
+        // On ajoute les détection d'évênements
+        this.inputElement.addEventListener('input', () => { this.autoSuggest(parentDiv, suggestionBox); });
+        this.inputElement.addEventListener('click', () => { this.autoSuggest(parentDiv, suggestionBox); });
 
         // On gère la sélection des suggestions avec les flèches directionnelles
         this.inputElement.addEventListener('keydown', (e) => {
@@ -71,9 +45,12 @@
             }
         });
 
-        // On ferme les suggestions si la souris clique ailleurs
+        // On ajoute les events de fermeture des suggestions
         document.addEventListener('click', (e) => {
             if (e.target !== this.inputElement) 
+                this.closeAllLists();
+        });
+        this.inputElement.addEventListener('blur', () => {
                 this.closeAllLists();
         });
     }
@@ -86,5 +63,43 @@
         const items = document.querySelectorAll('.autocomplete-items div');
         items.forEach(item => item.remove());
         this.currentFocus = -1;
+    }
+
+    autoSuggest(parentDiv, suggestionBox) {
+        console.log("L'élément a été sélectionné");
+        // On efface l'ancienne liste
+        this.closeAllLists();
+
+        // On génère le tableau de suggestions selon la saisie de l'utilisateur
+        let filteredSuggestions;
+        if(this.inputElement.value) {
+            console.log("L'utilisateur recherche les résultat commençant par : " + this.inputElement.value);
+            filteredSuggestions = this.suggestions.filter(suggestion =>
+                suggestion.toLowerCase().trim().startsWith(this.inputElement.value.toLowerCase().trim())
+            );
+
+        } else {
+            console.log("L'utilisateur n'a pas précisé sa recherche");
+            filteredSuggestions = Array.from(this.suggestions);
+        }   
+        console.log("Résultats proposés : ");
+        console.log(filteredSuggestions);
+
+        // On test la présence de résultat
+        if (filteredSuggestions.length > 0) {
+            suggestionBox.classList.add('autocomplete-items');
+
+            // On génère les items de suggestion
+            filteredSuggestions.forEach(suggestion => {
+                const item = document.createElement('div');
+                item.innerHTML = suggestion;
+                item.addEventListener('click', () => {
+                    this.inputElement.value = suggestion;
+                    this.inputElement.dispatchEvent(new Event('AutoCompleteSelect'));
+                    this.closeAllLists();
+                });
+                suggestionBox.appendChild(item);
+            });
+        }
     }
 }
